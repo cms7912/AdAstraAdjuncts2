dbdbb8fa11bec191b1fb204dff69574a4ff352b5
Clint Ramirez Stephens <github@clint-stephens.com>
2023-07-19 19:45:14 -0500

    Δ CMS iMac 2017 Wed, Jul 19 19:45:14 PM

--- Sources/AAQuantumValue/QuantumValue  .swift
+++ Sources/AAQuantumValue/QuantumValue  .swift
@@ -41,14 +41,20 @@ public enum QuantumValue: JSONCodable, Equatable, Hashable {
   // case array([QuantumValue])
   // case dictionary(Dictionary<QuantumValue,QuantumValue>)
 
+	#if Disabled
 	public func encode(to encoder: Encoder) throws {
 		var container = encoder.singleValueContainer()
 		if false { }
-		else if case let .int(value) = self { try container.encode(value) }
-		else if case let .double(value) = self { try container.encode(value) }
-		else if case let .string(value) = self { try container.encode(value) }
-		else if case let .bool(value) = self { try container.encode(value) }
-		else if case let .data(value) = self { try container.encode(value) }
+		else if case let .int(value) = self {
+			try container.encode(value) }
+		else if case let .double(value) = self {
+			try container.encode(value) }
+		else if case let .string(value) = self {
+			try container.encode(value) }
+		else if case let .bool(value) = self {
+			try container.encode(value) }
+		else if case let .data(value) = self {
+			try container.encode(value) }
 //		else { }
 		
 	}
@@ -56,21 +62,25 @@ public enum QuantumValue: JSONCodable, Equatable, Hashable {
 //		self.init(<#T##newValue: Quantumable##Quantumable#>)
 
 		if false { throw QuantumError.missingValue }
-		else if let v = try? decoder.singleValueContainer().decode(Int.self) { self.init(integerLiteral: v) }
-		else if let v = try? decoder.singleValueContainer().decode(Double.self) { self.init(floatLiteral: v) }
 		else if let v = try? decoder.singleValueContainer().decode(String.self) {
 			self.init(stringLiteral: v) }
+		else if let v = try? decoder.singleValueContainer().decode(Double.self) {
+			self.init(floatLiteral: v) }
+		else if let v = try? decoder.singleValueContainer().decode(Int.self) {
+			self.init(integerLiteral: v) }
 		// else if let v = try? decoder.singleValueContainer().decode(Date.self) { self = .date(v) }
-		else if let v = try? decoder.singleValueContainer().decode(Bool.self) { self.init(booleanLiteral: v) }
+		else if let v = try? decoder.singleValueContainer().decode(Bool.self) {
+			self.init(booleanLiteral: v) }
 		// else if let v = try? decoder.singleValueContainer().decode(UUID.self) { self = .uuid(v) }
-		else if let v = try? decoder.singleValueContainer().decode(Data.self) { self.init(dataLiteral: v)}
+		else if let v = try? decoder.singleValueContainer().decode(Data.self) {
+			self.init(dataLiteral: v)}
 		// else if let v = try? decoder.singleValueContainer().decode(JSONData.self) { self = .json(v) }
 		// else if let v = try? decoder.singleValueContainer().decode([QuantumValue].self) { self = .array(v) }
 		// else if let v = try? decoder.singleValueContainer().decode(Dictionary<QuantumValue,QuantumValue>.self) { self = .dictionary(v) }
 		else { throw QuantumError.missingValue }
 	}
+	#endif
 
-  
   public init(_ newValue: Quantumable) throws {
     // if false { fatalError()
     // } else if let v = newValue as? Int? { self = .int(v)
